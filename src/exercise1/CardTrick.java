c5835806145248df1a6bd2830cf86891fd96518d
BachBachBachBachBach <134092186+BachBachBachBachBach@users.noreply.github.com>
2023-05-26 14:30:14 -0400

    Change the print message

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -65,19 +65,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Bach and that's fine.");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- Get better salaries");
+        System.out.println("-- Save up for early retirement");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
+        System.out.println("-- DnD");
         System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Gaming");
+        System.out.println("-- Sleep");
 
         System.out.println();
         
